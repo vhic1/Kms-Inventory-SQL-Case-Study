23ebd315f970127a5d5f3111052d44a16ed39a07
Adesoye Victor <victoradesoye@gmail.com>
2025-07-04 17:35:31 +0100

    Delete KMS-Inventory-SQL-Case-Study directory

--- KMS-Inventory-SQL-Case-Study/Queries/01_Product_Category_With_Highest_Sales.sql
+++
@@ -1,7 +0,0 @@
----Question 1: Which product category had the highest sales?
-
-SELECT TOP 1 [Product_Category],
-SUM(Sales) AS Total_Sales
-FROM case_study
-GROUP BY [Product_Category]
-ORDER BY Total_Sales DESC;
--- KMS-Inventory-SQL-Case-Study/Queries/04_Bottom 10_Customers.sql
+++
@@ -1,7 +0,0 @@
----Question 4: Advise the management of KMS on what to do to increase revenue from the bottom 10 customers?
-
-SELECT TOP 10 [Customer_Name],
-SUM(sales) AS Total_Sales
-FROM Case_Study
-GROUP BY [Customer_Name]
-ORDER BY Total_Sales ASC;
