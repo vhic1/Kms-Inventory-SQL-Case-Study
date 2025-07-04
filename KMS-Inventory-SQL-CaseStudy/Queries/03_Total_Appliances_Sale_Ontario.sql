5710150ee6b79f8f3f274348a0f5f60daaaf9a4f
Adesoye Victor <108590880+vhic1@users.noreply.github.com>
2025-07-04 01:53:13 +0100

    Delete KMS-Inventory-SQL-CaseStudy directory

--- KMS-Inventory-SQL-CaseStudy/Queries/03_Total_Appliances_Sale_Ontario.sql
+++
@@ -1,6 +0,0 @@
----Question 3: What were the total sales of appliances in Ontario?
-
-SELECT Region, Product_Sub_Category, SUM (Sales) AS [Total_Sales]
-FROM Case_Study
-WHERE Region = 'Ontario' AND Product_Sub_Category = 'Appliances'
-GROUP BY Region, Product_Sub_Category
--- KMS-Inventory-SQL-CaseStudy/Queries/07_Small_Business_Customer_With_Highest_Sales.sql
+++
@@ -1,7 +0,0 @@
----Question 7: Which small business customer had the highest sales?
-
-SELECT TOP 1 Customer_Name, SUM(Sales) AS Total_Sales
-FROM Case_Study
-WHERE Customer_Segment = 'Small Business'
-GROUP BY Customer_Name
-ORDER BY Total_Sales DESC;
